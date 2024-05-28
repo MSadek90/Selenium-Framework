5d44101c991822f48dad29c89f273fe68384adf3
MSadek90 <mhmod.sadek2017@gmail.com>
2024-05-28 13:49:23 +0300

    "Edge Browser Initialization"

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Main {
+
+    public static void main(String[] args) {
+        WebDriver driver = new EdgeDriver();
+
+        
+
+    }
+
+
+}
